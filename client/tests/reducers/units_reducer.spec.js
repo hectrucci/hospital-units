311d3a7f5869c23ad5ea97c45bbbb7c194e63237
hantunez <hector.atz5@gmail.com>
2017-11-04 22:15:29 +0000

    Adding tests

---
+++ client/tests/reducers/units_reducer.spec.js
@@ -0,0 +1,28 @@
+import { units } from '../../src/store/reducers/units';
+
+describe('Testing Unit Reducer', () => {
+    it('Should set units to the state', () => {
+        const state = [
+            {
+                capacity: 18,
+                census: 2,
+                highAlarm: null,
+                id: '1',
+                lowAlarm: 4,
+                name: 'Intermediate',
+            },
+        ];
+
+        const action = {
+            type: 'SAVE_UNITS',
+            units: state,
+        };
+
+        const result = units(state, action);
+
+        expect(result.length).toEqual(1);
+        expect(result[0].name).toEqual('Intermediate');
+        expect(result[0].capacity).toEqual(18);
+        expect(result[0].lowAlarm).toEqual(4);
+    });
+});
